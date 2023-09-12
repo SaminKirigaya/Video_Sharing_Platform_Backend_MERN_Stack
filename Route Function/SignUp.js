88689994e9e9511f582ab4510891ceabac949701
SaminKirigaya <arnobsamin95@gmail.com>
2023-09-12 10:24:15 +0600

    first rout function for sign up

---
+++ Route Function/SignUp.js
@@ -0,0 +1,7 @@
+const VerifiedUsers = require('../Model/VerifiedUsers')
+const UsersInQuee = require('../Model/UsersInQuee')
+const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+const joi = require('joi')
+const jimp = require('jimp')
+
--- Route/Routes.js
+++ Route/Routes.js
@@ -3,4 +3,12 @@ const express = require('express')
 const Routes = express.Router()
 
 
+// Adding all route function
+const SignUp = require('../Route Function/SignUp')
+
+
+Routes.post('/signMeUp', 
+SignUp 
+)
+
 module.exports = Routes
